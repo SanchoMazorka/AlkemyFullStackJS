0dffcbbd025a36593e8cd7ddc15bc3863e6af5ca
SanchoMazorka <lito_hx@hotmail.com>
2022-04-22 05:10:00 -0300

    nav header with auth

--- client/src/ui/layout/Header.jsx
+++ client/src/ui/layout/Header.jsx
@@ -1,19 +1,42 @@
+import { useState, useContext } from "react"
+import { Link } from "react-router-dom"
+import TopMenu from "../pages/TopMenu"
+import { contextFromAuth } from '../../api/AuthProvider'
+
 const Header = () => {
+	const [test, setTest] = useState(false)
+	const { auth } = useContext(contextFromAuth)
   return (
     <header>
+			<button onClick={()=>setTest(!test)} value='PROBAR'/>
       <nav>
-        <img src='./../../../img/corn.png' width={36} height={36} />
-				<div className="user">
-          <span>Leonardo</span>
-          <span class='material-icons-outlined'>arrow_drop_down</span>
-        </div>
+        <img src='./../../../img/corn.png'/>
+
         <ul className='abm-menu'>
-          <il><span class='material-icons-outlined'>home</span></il>
-          <il><span class='material-icons-outlined'>add_circle_outline</span></il>
-          <il><span class='material-icons-outlined'>edit</span></il>
-          <il><span class='material-icons-outlined'>delete</span></il>
+          <li>
+						<span className='material-icons-outlined'>home</span>
+						<span className='icon-text'>HOME</span>
+					</li>
+          <li>
+						<span className='material-icons-outlined'>add_circle_outline</span>
+						<span className='icon-text'>AGREGAR</span>
+					</li>
+          <li>
+						<span className='material-icons-outlined'>edit</span>
+						<span className='icon-text'>EDITAR</span>
+					</li>
+          <li>
+						<span className='material-icons-outlined'>delete</span>
+						<span className='icon-text'>ELIMINAR</span>
+					</li>
         </ul>
+
+				<div className="user">
+          <span className="username"><Link to='/user'>{auth ? 'Leonardo':'LOGIN'}</Link></span>
+          <span className='material-icons-outlined'>arrow_drop_down</span>
+        </div>
       </nav>
+			{test && <TopMenu setTest={setTest}/>}
     </header>
   )
 }
