c33daeda5fe98efb26d61edaf332e5afb2f60973
SanchoMazorka <lito_hx@hotmail.com>
2022-05-09 04:40:22 -0300

    list transactions

--- client/src/ui/components/Table.jsx
+++ client/src/ui/components/Table.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
+import { useContext } from 'react'
+import { contextFromAuth } from '../../api/AuthProvider'
 
 const Table = () => {
+	const { auth, userData, userTransactions } = useContext(contextFromAuth)
+
+	const transformDate = (ms) => {
+		const date = new Date(ms*1000)
+		let fecha =  `${date.getHours().toString().padStart(2, 0)}:${date.getMinutes().toString().padStart(2, 0)} ${date.getDay().toString().padStart(2, 0)}/${date.getMonth().toString().padStart(2, 0)}/${date.getUTCFullYear().toString().padStart(2, 0)}`
+		console.log(fecha);
+		return fecha
+	}
+
 	return (
-		<table class='table'>
-			<thead>
-				<tr>
-					<th>OPERACIÓN</th>
-					<th>MONTO</th>
-					<th>FECHA</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-				</tr>
-				<tr>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-					<td>&nbsp;</td>
-				</tr>
-			</tbody>
-		</table>
-	)
+	<>
+		{userData!=undefined && userData.qt>0 ?
+			<table className='table'>
+				<thead>
+					<tr>
+						<th>OPERACIÓN</th>
+						<th>MONTO</th>
+						<th>CATEGORÍA</th>
+						<th>FECHA</th>
+					</tr>
+				</thead>
+				<tbody>
+					{userTransactions.map(data =>  
+						<tr key={data.id} style={{color: data.credit?'green':'red'}}>
+							<td>{data.credit?'CRÉDITO':'DÉBITO'}</td>
+							<td>${data.amount}</td>
+							<td>{data.cat.toUpperCase()}</td>
+							<td>{ transformDate(data.date.seconds) }</td>
+						</tr>
+					)}
+				</tbody>
+			</table>
+			:
+			<h2>NO HAY TRANSACCIONES PARA MOSTRAR.</h2>
+		}
+	</>)
 }
 
 export default Table
