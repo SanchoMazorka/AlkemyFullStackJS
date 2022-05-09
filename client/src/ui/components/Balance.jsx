d0706946d055fc083922904f57c86b1563e07ac4
SanchoMazorka <lito_hx@hotmail.com>
2022-05-09 04:49:35 -0300

    update text

--- client/src/ui/components/Balance.jsx
+++ client/src/ui/components/Balance.jsx
@@ -3,12 +3,13 @@ import { contextFromAuth } from '../../api/AuthProvider'
 
 const Balance = () => {
 	const { auth, userData } = useContext(contextFromAuth)
+
 	return (
 		<div className='balance'>
 			<span className="balance-name">BALANCE</span>
 			<div className='balance-amount'>
 				<span>$</span>
-				<span>{userData.hasOwnProperty('balance')?userData.balance:'CARGANDO...'}</span>
+				<span>{userData!=undefined?userData.balance:'CARGANDO...'}</span>
 			</div>
 		</div>
 	)
